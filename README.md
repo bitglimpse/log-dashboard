d904fb4a42ead947d4c18af5f75134388cc35229
bitglimpse <bitglimpse@gmail.com>
2019-12-11 07:32:24 -0700

    add readme content

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-TODO: write this
+# Node real-time log dashboard
+
+To start the application, in a terminal type: <code>npm start</code>
+
+Then, in a browser, go to: <code>http://localhost:3000/</code>
+
+Follow the <b>Select File</b> link and open the <b>log-generator>sample.log</b> file
--- log-generator/log-generator.js
+++ log-generator/log-generator.js
@@ -3,13 +3,13 @@
 const fs = require("fs")
 var path = require("path")
 
-var data = fs.readFileSync(path.join(__dirname, "./access.log"), "utf8")
+var data = fs.readFileSync(path.join(__dirname, "./sample-source.log"), "utf8")
 const sourceLogs = data.split("\n")
 
 var index = 0
 var count = 0
 // Initialize log file
-fs.writeFileSync(path.join(__dirname, "./generated-source.log"), sourceLogs[index++], "utf8");
+fs.writeFileSync(path.join(__dirname, "./sample.log"), sourceLogs[index++], "utf8");
 
 setInterval(() => {
     if (count === 50) { // This simulates log rotation
@@ -17,14 +17,14 @@ setInterval(() => {
         if (index === sourceLogs.length) {
             index = 0 // start at the beginning of the file
         }
-        fs.writeFileSync(path.join(__dirname, "./generated-source.log"), sourceLogs[index++], "utf8");
+        fs.writeFileSync(path.join(__dirname, "./sample.log"), sourceLogs[index++], "utf8");
         return
     }
-    const logs = fs.readFileSync(path.join(__dirname, "./generated-source.log"), "utf8");
+    const logs = fs.readFileSync(path.join(__dirname, "./sample.log"), "utf8");
     if (index === sourceLogs.length) {
         index = 0 // start at the beginning of the file
     }
-    fs.writeFileSync(path.join(__dirname, "./generated-source.log"), logs + "\n" + sourceLogs[index++], "utf8");
+    fs.writeFileSync(path.join(__dirname, "./sample.log"), logs + "\n" + sourceLogs[index++], "utf8");
     count++
 }, 2000);
 
